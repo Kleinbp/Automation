499ee976131a3abf971281fa9c66ba70e6bcc57e
Kleinbp <57073756+Kleinbp@users.noreply.github.com>
2022-08-06 23:04:54 +0200

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -1,43 +1,86 @@
-﻿# Script created by Philip Kleinberg #001510253
+#Scirpt cretaed by Philip Kleinberg #001510253
 
-#Varibles for script
-$folder_path = 'F:\Scripting and Automation\Requirements1' #folder location for requirements folder on my machine, if you have issues just change the location to YOUR requirements folder location
-$Cpu_load_percentage = Get-CimInstance Win32_Processor | Select-Object -Property LoadPercentage #loads CPU load %
-$Computer_info = Get-ComputerInfo #loads all computer info for RAM calculation
-$total_ram = ($Computer_info.OsTotalVirtualMemorySize)
-$free_ram = ($Computer_info.OsFreeVirtualMemory)
-$used_ram = ($total_ram - $free_ram)
-$ram_usage = ($used_ram/$total_ram*100)
+#module import
+Import-Module ActiveDirectory
 
+#OU creation
+New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=COM" 
 
+#Ou path
+$Domain = "OU=Finance,DC=consultingfirm,DC=com"
+
+#user list
+$NewUsersList = Import-csv C:\Users\LabAdmin\Desktop\Requirements2\financePersonnel.csv
+
+#try/catch statement to help stop potential issues with ForEach
 try {
 
+ForEach($user in $NewUsersList) {
 
-#do loop that allows continual choices by users to be made after switch statement 
-do {
+$First_Name = $user.First_Name.trim()
+$Last_Name = $user.Last_Name.trim()
+$samAccount = $user.samAccount.trim()
+$Display_name = "$First_Name" + " " + "$Last_Name"
+$City = $user.City.trim()
+$County = $user.County.trim()
+$PostalCode = $user.PostalCode.trim()
+$officephone = $user.OfficePhone.trim()
+$mobilephone = $user.MobilePhone.trim()
+$OU_path = "OU=Finance,DC=consultingfirm,DC=COM"
 
-#user option varible- allows user to input choice during loop
-$user_option_input = Read-Host "Please select an option:
 
-Enter 1 to generate daily log
 
-Enter 2 to list files in requirements folder
+New-ADUser -Path $OU_path -Name "$Display_name" -Enabled $true -PostalCode "$PostalCode" -MobilePhone "$mobilephone" -City "$City" -OfficePhone "$officephone" -DisplayName "$Display_name" -GivenName "$First_Name" -Surname "$Last_Name" -SamAccountName "$samAccount" -PasswordNotRequired $true 
 
-Enter 3 to list current CPU and memory usage
 
-Enter 4 to list current running processes 
 
-Enter 5 to exit script"
+} } Catch [System.OutOfMemoryException] {Write-Host "Out of memory exception has occured."}
 
-#Good ol switch statement allowing choices to be made during loop
-switch ($user_option_input) {
+#Sql server import
+Import-module -Name "SqlServer"
 
-1 {Get-Date | Out-File -FilePath $folder_path'\DailyLog.txt' -Append; Get-ChildItem $folder_path -Filter *.log | Select-Object -Property Name | Format-Table -HideTableHeaders | Out-File -Append -FilePath $folder_path'\DailyLog.txt'; Clear-host; write-host -ForegroundColor Red "Generating log"}
-2 {Get-ChildItem $folder_path | Select-Object -Property Name, CreationTime, Length | Sort-Object -Property Name | Format-Table -HideTableHeader | Out-file -LiteralPath "$folder_path\C916contents.txt" ; clear-host ; Write-Host -ForegroundColor Red "Generating C916 file"}
-3 {Clear-host; Write-Host -ForegroundColor Red "Current CPU usage is"($Cpu_load_percentage.LoadPercentage)"%"; Write-Host -ForegroundColor Red "Ram usage is" $ram_usage"%"}
-4 {Get-Process | Select-Object -Property VirtualMemorySize, Name | Sort-Object -Property VirtualMemorySize | Out-GridView; Clear-Host}
-default {} } 
-} until ($user_option_input -eq 5) #allows loop to exit when user picks choice 5
+#Import contacts from CSV
+$CSV_import = Import-csv C:\Users\LabAdmin\Desktop\Requirements2\NewClientData.csv
 
-} Catch [System.OutOfMemoryException] {Write-Host "Out of memory exception has occured."}
+#ClientDB creation
+Invoke-Sqlcmd -ServerInstance "SRV19-Primary\SQLExpress" -Query "USE master;
+GO
+CREATE DATABASE ClientDB
+GO"
 
+#Client_A_Contacts Table creation
+Invoke-Sqlcmd -ServerInstance "SRV19-Primary\SQLExpress" -Database "ClientDB" -Query "USE ClientDB
+    CREATE TABLE Client_A_Contacts (
+    first_name varchar(255),
+    last_name varchar(255),
+    city varchar(255),
+    county varchar(255),
+    zip varchar(255),
+    officePhone varchar(255),
+    mobilePhone varchar(255)
+);"
+
+#try/catch statement to help stop potential issues with ForEach
+try {
+
+#Importing CSV into Contacts table
+Foreach ($csv_line in $CSV_import) {
+$first_name = $csv_line.first_name.trim()
+$last_name = $csv_line.last_name.trim()
+$city = $csv_line.city.trim()
+$county = $csv_line.county.trim()
+$zip = $csv_line.zip.trim()
+$officePhone = $csv_line.officePhone.trim()
+$mobile = $csv_line.mobilePhone.trim()
+
+Invoke-Sqlcmd -ServerInstance "SRV19-Primary\SQLExpress" -Database "ClientDB" -Query "USE ClientDB
+INSERT INTO dbo.Client_A_Contacts (first_name, last_name, city, county, zip, officePhone, mobilePhone)
+VALUES('$first_name', '$last_name', '$city', '$county', '$zip', '$officePhone', '$mobile');"
+
+
+
+} } Catch [System.OutOfMemoryException] {Write-Host "Out of memory exception has occured."}
+
+#Outputting SQL results
+cd C:\Users\LabAdmin\Desktop\
+Invoke-sqlcmd -Database ClientDB -ServerInstance .\SQLEXPRESS -Query 'SELECT * FROM dbo.Client_A_Contacts' > .\results.txt
